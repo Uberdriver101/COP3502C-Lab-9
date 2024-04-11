5384d79d30c62cdb9e862a0631926b9d057373d6
Lucas <lucasaguirre0605@gmail.com>
2024-04-10 22:06:57 -0400

    committed my liege

--- Lab9.py
+++ Lab9.py
@@ -1,4 +1,10 @@
 
+#def encode written by Alexander
+def encoder(string):
+    pass
+
+
+
 
 #decoder created by Lucas
 def decoder(string):
@@ -8,13 +14,10 @@ def decoder(string):
         number -= 3
         #accounts for numbers larger than 6 that return double digit numbers
         if number < 0:
-            number += 10
+                        number += 10
         number = str(number)
         decoded_message += number
     return decoded_message
 
 print(decoder('90123'))
 
-#def encode written by Alexander
-def encoder(string):
-    pass
