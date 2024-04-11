61cef345757b4645c734cc669b6674d680a18b4a
Uberdriver101 <alexshen066@gmail.com>
2024-04-10 22:03:23 -0400

    mommy milkers

--- Lab9.py
+++ Lab9.py
@@ -1,16 +1,1 @@
-print('Heyy!!')
 
-#decoder created by Lucas
-def decoder(string):
-    decoded_message = ''
-    for i in range(len(string)):
-        number = int(string[i])
-        number -= 3
-        #accounts for numbers larger than 6 that return double digit numbers
-        if number < 0:
-            number += 10
-        number = str(number)
-        decoded_message += number
-    return decoded_message
-
-print(decoder('90123'))
