0ec620b4c281cfda5d0f3f9dab4dd73faae22efa
Lucas <lucasaguirre0605@gmail.com>
2024-04-10 21:58:24 -0400

    committed my liege

--- Lab9.py
+++ Lab9.py
@@ -1,1 +1,16 @@
 print('Heyy!!')
+
+#decoder created by Lucas
+def decoder(string):
+    decoded_message = ''
+    for i in range(len(string)):
+        number = int(string[i])
+        number -= 3
+        #accounts for numbers larger than 6 that return double digit numbers
+        if number < 0:
+            number += 10
+        number = str(number)
+        decoded_message += number
+    return decoded_message
+
+print(decoder('90123'))
